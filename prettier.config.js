c67f8541fbbb52047abf1bb0e79b36bb72e67b22
Alberto José <albertorauljose2@gmail.com>
2025-09-20 14:23:42 +0100

    style(prettier): install and configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('prettier').Config}
+ */
+export default {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  quoteProps: 'as-needed',
+};
